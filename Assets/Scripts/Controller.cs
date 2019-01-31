69936de32ccf3e5b3e01cdd818d98c4b6c991ae6
Herman Martinus <hfbmartinus@gmail.com>
2019-01-31 09:10:07 +0200

    Initializing repo

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+
+    public float Player1X = 0;
+    public float Player2X = 0;
+
+    public static Controller Instance;
+
+    // Use this for initialization
+    void Awake () {
+        Instance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0) && Input.mousePosition.x < Screen.width /2f )
+        {
+            Player1X = Clamp((Input.mousePosition.x / (Screen.width / 2) - 0.5f) * 2);
+        }
+        else
+        {
+            Player1X = 0;
+        }
+
+        if (Input.GetMouseButton(0) && Input.mousePosition.x > Screen.width / 2f)
+        {
+            Player2X = Clamp((Input.mousePosition.x / (Screen.width / 2) - 1.5f) * 2);
+        }
+        else
+        {
+            Player2X = 0;
+        }
+
+        MobileController();
+    }
+
+    void MobileController()
+    {
+        if (Input.touchCount > 0)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                //For left half screen
+                if (touch.phase == TouchPhase.Began && touch.position.x <= Screen.width / 2)
+                {
+                    Player1X = Clamp((Input.mousePosition.x / (Screen.width / 2) - 0.5f) * 2);
+                }
+                //For right half screen
+                if (touch.phase == TouchPhase.Began && touch.position.x > Screen.width)
+                {
+                    Player2X = Clamp((Input.mousePosition.x / (Screen.width / 2) - 1.5f) * 2);
+                }
+                if (touch.phase == TouchPhase.Ended)
+                { //correct end of touch
+                    //if (touch.fingerId == finId1)
+                    //{ //check id finger for end touch
+                    //    finId1 = -1;
+                    //}
+                    //else if (touch.fingerId == finId2)
+                    //{
+                    //    finId2 = -1;
+                    //}
+                }
+            }
+        }
+    }
+
+    float Clamp(float input)
+    {
+        if(input > 1 || input < -1)
+        {
+            return 0;
+        }
+        return input;
+    }
+}
---
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    public static PlayerPlatformerController Instance;
+
+    public bool hasMoved = false;
+
+    public bool player1 = true;
+
+    // Use this for initialization
+    void Awake()
+    {
+        Instance = this;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+
+        Vector2 move = Vector2.zero;
+
+        if (player1)
+        {
+            move.x = Controller.Instance.Player1X;
+        }
+        else
+        {
+            move.x = Controller.Instance.Player2X;
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        { 
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        if(Mathf.Abs(move.x) > 0.01f)
+        {
+            spriteRenderer.flipX = move.x < 0;
+            if (!hasMoved)
+            {
+                hasMoved = true;
+            }
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+}
